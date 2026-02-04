68b4f4b0a0fd80eba868b9a929019796f097b52c
Aniket Rajendra Upase <aniketu0807@gmail.com>
2026-02-04 20:15:46 +0530

    Add files via upload

---
+++ translate_service.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class TranslatorService {
+  // Use localhost for Flutter Web
+  static String baseUrl = "https://translator-backend-z3q4.onrender.com";
+
+  Future<String> translateText(String text, String targetLang) async {
+    try {
+      debugPrint("Sending to backend: $text → $targetLang"); // Debug print
+
+      final response = await http.post(
+        Uri.parse("$baseUrl/translate"),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({"text": text, "target_lang": targetLang}),
+      );
+
+      debugPrint(
+        "Response status: ${response.statusCode}, body: ${response.body}",
+      ); // Debug print
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return data["translated_text"] ?? "No translation found";
+      } else {
+        return "Error: ${response.statusCode}";
+      }
+    } catch (e) {
+      debugPrint("Exception: $e");
+      return "Error: $e";
+    }
+  }
+
+  Future<Map<String, dynamic>> translateFile(File imageFile, String targetLang) async {
+    try {
+      var request = http.MultipartRequest(
+        'POST',
+        Uri.parse("$baseUrl/file_translate"),
+      );
+
+      request.fields['target_lang'] = targetLang;
+      request.files.add(await http.MultipartFile.fromPath('file', imageFile.path));
+
+      var response = await request.send();
+      var responseData = await response.stream.bytesToString();
+
+      return json.decode(responseData);
+    } catch (e) {
+      return {"error": "Connection failed: $e"};
+    }
+  }
+}
